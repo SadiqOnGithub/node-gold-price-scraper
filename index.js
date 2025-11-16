8662e6d341cd023d416c24dc0a252c9385191827
sadiq_from_thinkpad <sadiqonemail@gmail.com>
2025-11-16 07:36:07 +0530

    conbazaar added with other improvement

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-const fs = require("fs");
+import fs from "fs";
 
+// ajio
 fetch("https://www.ajio.com/search/?text=mmtc%20pamp%201gm")
   .then((response) => response.text())
   .then((text) => {
@@ -26,6 +27,7 @@ fetch("https://www.ajio.com/search/?text=mmtc%20pamp%201gm")
     console.log("=====================================");
   });
 
+// mmtc-pamp
 fetch("https://www.mmtcpamp.com/shop/gold/lotus-gold-bar-24k-1gm")
   .then((response) => response.text())
   .then((data) => {
@@ -51,10 +53,13 @@ fetch("https://www.mmtcpamp.com/shop/gold/lotus-gold-bar-24k-1gm")
       console.log("Could not find mmtc-pamp data");
     }
   })
-  .then(() => {
+
+  .catch((error) => console.error("Error:", error))
+  .finally(() => {
     console.log("=====================================");
   });
 
+// Amazon
 fetch("https://www.amazon.in/MMTC-PAMP-Lotus-24k-999-9-Gold/dp/B08KSPYJX2/", {
   headers: {
     "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.0.0 Safari/537.36",
@@ -78,4 +83,43 @@ fetch("https://www.amazon.in/MMTC-PAMP-Lotus-24k-999-9-Gold/dp/B08KSPYJX2/", {
       console.log("Could not find price data");
     }
   })
-  .catch(console.error);
+  .catch((error) => console.error("Error:", error))
+  .finally(() => {
+    console.log("=====================================");
+  });
+
+import mynta from "./coin_sites/mynta.js";
+mynta();
+
+// ==========================
+// conbzaar is giving cloudflare protection, can't scrape the data
+// ==========================
+// fetch("https://www.coinbazaar.in/1-gram-gold-coins-price-1gm-gold-bar-todays-rate-/3107-mmtc-pamp-gold-lotus-bar-of-1-grams-24-karat-in-9999-purity-fineness-in-certi-card.html", {
+//   headers: {
+//     "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.0.0 Safari/537.36",
+//     Accept: "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+//     "Accept-Language": "en-IN,en;q=0.9",
+
+//     // Critical for Cloudflare/browser navigation
+//     "Sec-Fetch-Site": "none",
+//     "Sec-Fetch-Mode": "navigate",
+//     "Sec-Fetch-User": "?1",
+//     "Sec-Fetch-Dest": "document",
+//     "Upgrade-Insecure-Requests": "1",
+//     Referer: "https://www.google.com/",
+//   },
+// })
+//   .then((response) => response.text())
+//   .then((text) => {
+//     console.log("---CoinBazaar---");
+//     fs.writeFileSync("data.html", text);
+
+//     const re = /<div[^>]*class="[^"]*twister-plus-buying-options-price-data[^"]*"[^>]*>\s*([\s\S]*?)\s*<\/div>/i;
+//     const match = text.match(re);
+//     if (match && match[1]) {
+//       const parsedData = JSON.parse(match[1]);
+//     } else {
+//       console.log("Could not find price data");
+//     }
+//   })
+//   .catch(console.error);
